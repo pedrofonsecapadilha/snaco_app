c4fcadd2c951bdd07de202ddc4ab7207b17197d6
pedrofpadilha <pedrofpadilha@gmail.com>
2023-08-10 22:05:49 -0300

    insumos: adjusts + colaboradores: all

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  def home
+  end
+
   def insumos
+  end
+  
+  def colaboradores
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root to: "pages#insumos"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+
+  get "/home", to: "pages#home", as: "home"
+  get "/insumos", to: "pages#insumos", as: "insumos"
+  get "/colaboradores", to: "pages#colaboradores", as: "colaboradores"
 end
